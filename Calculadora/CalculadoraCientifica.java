434cc892c176737dce93608b2ef7597bd1c4e536
Yann Gabriel <yanncarvalho@outook.com>
2021-11-18 18:28:21 -0300

    adiciona comandos de calculadora cientifica

---
+++ Calculadora/CalculadoraCientifica.java
@@ -0,0 +1,8 @@
+package com.calculadora;
+
+class CalculadoraCientifica extends Calculadora {
+
+    public double exponential (double value1, double value2)  {
+        return  Math.pow(value1, value2);
+    }
+}
