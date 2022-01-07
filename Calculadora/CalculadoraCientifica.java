f59caedf2c61c77d99e600c3c4df46ea7f5e28c5
Yann Gabriel Freire de Carvalho <94571297+yanngcarvalho@users.noreply.github.com>
2022-01-07 19:00:40 -0300

    Delete Calculadora directory

--- Calculadora/CalculadoraCientifica.java
+++
@@ -1,8 +0,0 @@
-package com.calculadora;
-
-class CalculadoraCientifica extends Calculadora {
-
-    public double exponential (double value1, double value2)  {
-        return  Math.pow(value1, value2);
-    }
-}
